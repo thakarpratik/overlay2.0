430bee5af3027c3b2908c14f6baa6609c74ed15d
thakarpratik <thakarpratik@gmail.com>
2026-02-01 00:52:22 -0500

    Import TemplateGallery in ChangeTemplateDrawer

--- components/editor/ChangeTemplateDrawer.tsx
+++ components/editor/ChangeTemplateDrawer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { templates } from "@/lib/templates";
+import TemplateGallery from "@/components/templates/TemplateGallery";
 import { useRouter, useSearchParams } from "next/navigation";
 
 export default function ChangeTemplateDrawer({ projectId }: { projectId: string }) {
