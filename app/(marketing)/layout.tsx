7a936f4b2a355095ee464eb788e9992a0ca55ff4
thakarpratik <thakarpratik@gmail.com>
2026-02-01 23:15:24 -0500

    Conditionally render Navbar based on pathname

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import Navbar from "@/components/marketing/Navbar";
 import Footer from "@/components/marketing/Footer";
+import { usePathname } from "next/navigation";
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isHomepage = pathname === '/';
+
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
+      {!isHomepage && <Navbar />}
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
